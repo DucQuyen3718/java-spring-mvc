2698e913125cc590e95792a773eda985e74e1ed8
DucQuyen3718 <phamduc3718@gmail.com>
2025-10-23 23:16:04 +0700

    #45 Áp dụng mô hình MVC

---
+++ src/main/java/vn/hecquyn/laptopshop/service/UserService.java
@@ -0,0 +1,11 @@
+package vn.hecquyn.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String handleHello() {
+        return "Hello from Service";
+    }
+}
