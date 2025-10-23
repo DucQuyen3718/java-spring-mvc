1e6a5272b2d5b902ad5e43cc9578b6b79e6b6ad3
DucQuyen3718 <phamduc3718@gmail.com>
2025-10-24 00:08:29 +0700

    #45 Áp dụng mô hình MVC

---
+++ src/main/java/vn/hecquyn/laptopshop/controller/UserController.java
@@ -0,0 +1,29 @@
+package vn.hecquyn.laptopshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hecquyn.laptopshop.service.UserService;
+
+// @Controller
+// public class UserController {
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello from controller";
+//     }
+// }
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+
+}
