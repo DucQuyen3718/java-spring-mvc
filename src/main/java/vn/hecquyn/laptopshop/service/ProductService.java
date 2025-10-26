2bef2123578a5fdd475ce3df8a59b108e60f3dc8
DucQuyen3718 <phamduc3718@gmail.com>
2025-10-27 00:20:50 +0700

    #104 - Update Delete Product

--- src/main/java/vn/hecquyn/laptopshop/service/ProductService.java
+++ src/main/java/vn/hecquyn/laptopshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package vn.hecquyn.laptopshop.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -23,4 +24,13 @@ public class ProductService {
     public List<Product> fetchProducts() {
         return this.productRepository.findAll();
     }
+
+    public Optional<Product> fetchProductById(long id) {
+        return this.productRepository.findById(id);
+    }
+
+    public void deleteProduct(long id) {
+        this.productRepository.deleteById(id);
+    }
+
 }
